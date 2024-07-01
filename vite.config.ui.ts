5b0902f7ff1783f9d969f0cb829271cd48b65bb9
Dmitry Kapitonov <dimkapitonov@yandex.ru>
2024-07-01 08:53:55 +0000

    add dev script and dev tools

--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -1,11 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  plugins: [
+    vue(),
+    viteSingleFile(),
+    vueDevTools()
+  ],
   build: {
     emptyOutDir: false,
     rollupOptions: {
